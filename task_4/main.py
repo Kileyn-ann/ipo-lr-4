db862a8a418a5176352f8a5c0f7f7798006fd666
Лиза <lizadan2008.s4@gmail.com>
2025-11-10 13:33:21 +0300

    Update main.py

--- task_4/main.py
+++ task_4/main.py
@@ -1,1 +1,6 @@
-
+import math #подключаем модуль math
+import random
+rand = [round(random.uniform(0,10),2)  for i in range(7)] #Создаём список чисел с 1 по 19
+print(rand)
+pow_l = [math.floor(num)  for num in rand]
+print(pow_l) #Выводим ноовый список
